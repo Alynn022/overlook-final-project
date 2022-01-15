7c017c80173bab3bedd7165a4f9c26c2fbf17db2
Lynn Nguyen <AmandaN22@outlook.com>
2022-01-15 16:56:27 -0700

    Started customer class and test-suites

---
+++ src/classes/Customer.js
@@ -0,0 +1,9 @@
+
+class Customer {
+  constructor(user) {
+    this.id = user.id;
+    this.name =  user.name;
+  }
+}
+
+export default Customer; 
--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,33 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
-
+import { fetchApiData } from './apiCalls';
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+//GET ELEMENTS BY ID 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+
+
+
+
+const loadPage = () => {
+  suggestedRecipe.innerHTML = '';
+  getData()
+  .then((data) => {
+    let randomUser = data[0][getRandomIndex(data[0])];
+    newUser = new User(randomUser);
+    rooms = new Rooms(data[1]);
+    bookings = new Bookings(data[2]);
+  })
+};
+
+const getData = () => {
+  return Promise.all([fetchApiData('customers'), fetchApiData('rooms'), fetchApiData('bookings')])
+};
+
+//EVENT LISTENERS
+
+window.addEventListener('load', loadPage);
