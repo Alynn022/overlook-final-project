ba49f25d7265ff7a15635aedf75b319f7eb9ed57
Lynn Nguyen <AmandaN22@outlook.com>
2022-01-17 19:29:08 -0700

    Added images to room display and button for book a room

--- src/classes/Bookings.js
+++ src/classes/Bookings.js
@@ -1,5 +1,9 @@
 import { fetchApiData } from '../apiCalls';
 import Rooms from './Rooms';
+import '../images/Junior-Suite.jpg'
+import '../images/Residential-Suite.jpg'
+import '../images/Single-Room.jpg'
+import '../images/Suite.jpg'
 
 class Bookings {
   constructor(bookingsData) {
@@ -16,6 +20,7 @@ class Bookings {
     .then((data) => {
       let bookings = new Bookings(data[0].bookings)
       let rooms = new Rooms(data[1].rooms)
+      console.log('rooms', rooms.roomsInformation)
       let roomsWithSameDate = bookings.bookingsData.filter((booking) => {
         if (userInput === booking.date) {
           return booking
@@ -29,11 +34,52 @@ class Bookings {
         results.forEach((elem) => {
           if (!this.roomTypesAvailable.includes(elem.roomType))
           this.roomTypesAvailable.push(elem.roomType)
-        })
-        })
+      })
+    })
+  }
+  assignImageToRoomType() {
+    this.roomsAvailable.forEach((elem) => {
+      if (elem.roomType === 'residential suite') {
+        let residentialSuiteImage = '../images/Residential-Suite.jpg'
+        elem['image'] = residentialSuiteImage
+        
       }
-    }
+      if (elem.roomType === 'junior suite') {
+        let juniorSuiteImage = '../images/Junior-Suite.jpg'
+        elem['image'] = juniorSuiteImage
+        
+      }
+      if (elem.roomType === 'single room') {
+        let singleRoomImage = '../images/Single-Room.jpg'
+        elem['image'] = singleRoomImage
+        
+      }
+      if (elem.roomType === 'suite') {
+        let suiteImage = '../images/Suite.jpg'
+        elem['image'] = suiteImage
+      }
+    })
+  }
+}
 
-
+// if (room.roomType === 'residential suite') {
+//   roomsAvailableDisplay.innerHTML += `<img src='./images/Residential-Suite.jpg'>
+//   <h1>Room Type: ${room.roomType} Has a Bidet: ${room.bidet}
+//   Bed Size: ${room.bedSize} Number Of Beds: ${room.numBeds}</h1>`
+//   }
+//   if (room.roomType === 'junior suite') {
+//     roomsAvailableDisplay.innerHTML += `<img src='./images/Junior-Suite.jpg'>
+//     <h1>Room Type: ${room.roomType} Has a Bidet: ${room.bidet}
+//     Bed Size: ${room.bedSize} Number Of Beds: ${room.numBeds}</h1>`
+//   }
+//   if (room.roomType === 'single room') {
+//     roomsAvailableDisplay.innerHTML += `<img src='./images/Single-Room.jpg'>
+//     <h1>Room Type: ${room.roomType} Has a Bidet: ${room.bidet}
+//     Bed Size: ${room.bedSize} Number Of Beds: ${room.numBeds}</h1>`
+//   }
+//   if (room.roomType === 'suite') {
+//     roomsAvailableDisplay.innerHTML += `<img src='./images/Suite.jpg'>
+//     <h1>Room Type: ${room.roomType} Has a Bidet: ${room.bidet}
+//     Bed Size: ${room.bedSize} Number Of Beds: ${room.numBeds}</h1>`
 
 export default Bookings;
--- src/scripts.js
+++ src/scripts.js
@@ -9,11 +9,15 @@ import Customer from './classes/Customer';
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/Junior-Suite.jpg'
+// import './images/Residential-Suite.jpg'
+// import './images/Single-Room.jpg'
+// import './images/Suite.jpg'
+
 
 //GET ELEMENTS BY ID 
+// const allRoomBookings = document.getElementById('allRoomBookings')
 const customerDashboardView = document.getElementById('customerDashboardView')
-const allRoomBookings = document.getElementById('allRoomBookings')
 const pastReservationsSection = document.getElementById('pastReservationsSection')
 const upcomingReservationsSection = document.getElementById('upcomingReservationsSection')
 const totalAmountSpentDisplay = document.getElementById('totalAmountSpentDisplay')
@@ -23,7 +27,12 @@ const dateSelectBtn = document.getElementById('dateSelectBtn')
 const roomsAvailableDisplay = document.getElementById('roomsAvailableDisplay')
 const dropDownButton = document.getElementById('dropDownButton');
 const myDropdown = document.getElementById("myDropdown");
+const bookThisRoomBtn = document.getElementById("bookThisRoomBtn")
 
+const roomsAvailableTitle = document.getElementById(".rooms-available-title")
+
+
+var dateControl = document.querySelector('input[type="date"]');
 
 let customer;
 let bookings;
@@ -66,7 +75,6 @@ const displayBookARoomCalendar = () => {
   showBookARoomView();
 }
 
-var dateControl = document.querySelector('input[type="date"]');
 
 const disableSubmitButton = () => {
   if (!dateControl.value) {
@@ -81,37 +89,54 @@ const displayBookARoomInformation = () => {
   roomsAvailableDisplay.innerHTML = `` 
   let splitDate = dateControl.value.split("-")
   let joinDate = splitDate.join("/")
-  bookings.showCustomerRoomAvailability(joinDate).then(() => {
-    if (bookings.roomsAvailable) {
-      bookings.roomsAvailable.forEach((room) => {
-        roomsAvailableDisplay.innerHTML += `<h1>Room Type: ${room.roomType} Has a Bidet: ${room.bidet}
-        Bed Size: ${room.bedSize} Number Of Beds: ${room.numBeds}</h1>`
+    bookings.showCustomerRoomAvailability(joinDate).then(() => {
+      if (bookings.roomsAvailable) {
+        bookings.assignImageToRoomType();
+        bookings.roomsAvailable.forEach((room) => {
+          roomsAvailableDisplay.innerHTML += `<section><img src=${room.image} style="float:left">
+          <p>Room Type: ${room.roomType}</p> 
+          <p>Has a Bidet: ${room.bidet}</p>
+          <p>Bed Size: ${room.bedSize}</p>
+          <p>Number Of Beds: ${room.numBeds}</p> 
+          <p>Cost Per Night: $${room.costPerNight}</p>
+          <button id="bookThisRoomBtn">Book This Room</button></section>`
       })
-    }
-    if (!bookings.roomsAvailable) {
-      roomsAvailableDisplay.innerHTML += `<h1>We are so sorry!! No Rooms are available for this date... Please try a different date!</h1>`
+      if (!bookings.roomsAvailable) {
+        roomsAvailableDisplay.innerHTML += `<h1>We are so sorry!! No Rooms are available for this date... Please try a different date!</h1>`
+      }
     }
   })
 }
+
 
 const showDropDown = () => {
   myDropdown.innerHTML = ``
   myDropdown.classList.toggle("show");
   bookings.roomTypesAvailable.forEach((roomType) => {
-    myDropdown.insertAdjacentHTML('afterBegin', `<a class ="${roomType}" href="#${roomType}">${roomType.toUpperCase()}</a>`)
+    myDropdown.innerHTML += `<a class ="${roomType}" href="#${roomType}">${roomType.toUpperCase()}</a>`
   })
 };
 
 const searchByRoomTypes = (event) => {
-  console.log(event.target.className)
   event.preventDefault();
+  myDropdown.innerHTML = ``
   roomsAvailableDisplay.innerHTML = ``
+  bookings.assignImageToRoomType();
   bookings.roomsAvailable.forEach((roomType) => {
     if (event.target.className === roomType.roomType) {
-      roomsAvailableDisplay.innerHTML += `<h1>Room Type: ${roomType.roomType} Has a Bidet: ${roomType.bidet}
-        Bed Size: ${roomType.bedSize} Number Of Beds: ${roomType.numBeds}</h1>`
+      roomsAvailableDisplay.innerHTML += `<section><img src=${roomType.image} style="float:left">
+      <p>Room Type: ${roomType.roomType}</p> 
+      <p>Has a Bidet: ${roomType.bidet}</p>
+      <p>Bed Size: ${roomType.bedSize}</p>
+      <p>Number Of Beds: ${roomType.numBeds}</p> 
+      <p>Cost Per Night: $${roomType.costPerNight}</p>
+      <button id="bookThisRoomBtn">Book This Room</button></section>`
     }
   })
+}
+
+const bookThisRoomPost = () => {
+
 }
 
 //HELPER FUNCTIONS
@@ -125,7 +150,7 @@ const hide = (elements) => {
 
 const showBookARoomView = () => {
   show([bookARoomView])
-  hide([customerDashboardView])
+  hide([customerDashboardView, bookARoomBtn])
 }
 
 //EVENT LISTENERS
@@ -135,3 +160,4 @@ bookARoomBtn.addEventListener('click', displayBookARoomCalendar);
 dateSelectBtn.addEventListener('click', displayBookARoomInformation);
 myDropdown.addEventListener('click', searchByRoomTypes);
 dropDownButton.addEventListener('click', showDropDown);
+bookThisRoomBtn.addEventListener('click', bookThisRoomPost)
